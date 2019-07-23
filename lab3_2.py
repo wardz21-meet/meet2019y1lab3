a50ffbb198efecbe085346bda567d93f1d7718f7
wardz21-meet <wardz21@meet.mit.edu>
2019-07-23 17:11:55 +0300

    Stopping here

---
+++ lab3_2.py
@@ -0,0 +1,3 @@
+response=input("Hello there, what is your name?")
+print("Nice to meet you, " + response.capitalize())
+answer=input("fun fact, your name has " + str(len(response)) + " charecters!. btw, what is your name?")
---
+++ lab_3.py
@@ -0,0 +1,8 @@
+user=input("Say Something")
+
+person_one=input("What is the age of the first person?")
+person_two=input("What is the age of the second verson?")
+k=int(person_one)
+l=int(person_two)
+m=(k-l)
+print("Your age difference is " + str(abs(m)))
